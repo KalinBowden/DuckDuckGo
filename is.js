5c228821aca0eef469ed1398b35559e79949ac7d
kalinbowden <kalinbowden@gmail.com>
2017-09-18 09:53:08 -0500

    class

--- is.js
+++ is.js
@@ -6,41 +6,47 @@ var title = ["Ballerina", "CowBoy", "Pirate", "Sucba Diver"];
 var desc = ["The Ballerina goes SHHHHHHHHH", "The Cowboy goes Theres a Snake in My Boot","The Pirate goes Arrrggghhhh", "The Scuba Diver goes Blub Blub Blub"];
   
 
-// I am grabbing the elements by their ID's and storing them in DOM variables
+// Get and Set the main image
 var main = document.getElementById("main");
+main.src = ducky[0];
+
+// Creat a timer that calls a image swap function
+myTimer = setInterval(swapRandomImage, 1000);
 
 
-// As I understood the instructions I was allowed to have a signle method that must be shared by all four 
-// of my IMG's. The idea here was when this method fired it would figure out which image was clicked and 
-// set the source of the larger image using the array.
-// Finding a way to determine which img was clicked from a single method is where I am stuck at.
+// fill the images that the child will select from
+function fillImages()
+{
+    for (var index = 0; index < ducky.length; index++)
+        {
+            document.getElementById("thumb-" + index).src = ducky[index];
+        }
+}
+
+// This will allow the child to pic an image
 function swapImages(pic)
 {
     //
-    if (pic === 0)// Only this if and the one below it are working. Once these two work I will add the other two.
-    {
-        main.src = ducky[pic];
-        mainTitle.innerHTML = title[pic];
-        mainPara.innerHTML = desc[pic];
-    }
-    else if (pic === 1)// I had attempted .click and clicked with no luck
-    {
-        main.src = ducky[pic];
-        mainTitle.innerHTML = title[pic];
-        mainPara.innerHTML = desc[pic];
-    }
-    else if (pic === 2)
-    {
-        main.src = ducky[pic];
-        mainTitle.innerHTML = title[pic];
-        mainPara.innerHTML = desc[pic];
-    }
-    else if (pic === 3)
-    {
-        main.src = ducky[pic];
-        mainTitle.innerHTML = title[pic];
-        mainPara.innerHTML = desc[pic];
-    }
-
-
+    main.src = ducky[pic];
+    mainTitle.innerHTML = title[pic];
+    mainPara.innerHTML = desc[pic];
 }
+
+// This will allow the page to randomly display images
+function swapRandomImage()
+{
+    var rand = Math.floor(Math.random() * 4);
+    main.src = ducky[rand];
+    mainTitle.innerHTML = title[rand];
+    mainPara.innerHTML = desc[rand];
+}
+
+// When the main image is clicked it will stop looping
+function endRandomSwap()
+{
+    clearInterval(myTimer);
+}
+
+// Event listenres
+window.addEventListener("load", fillImages, false);
+main.addEventListener("click", endRandomSwap, false);
